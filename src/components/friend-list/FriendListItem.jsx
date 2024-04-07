8e3a957faab6ad527caa782d9de05afb4ed5182d
Bohdan2k23 <130351031+Bohdan2k23@users.noreply.github.com>
2024-04-07 20:02:06 +0200

    .

--- src/components/friend-list/FriendListItem.jsx
+++ src/components/friend-list/FriendListItem.jsx
@@ -1,10 +1,9 @@
-import css from "./FriendList.module.css";
-export default function FriendListItem({ avatar, name, isOnline, id }) {
+export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p className={isOnline ? "text-green" : "text-red"}>
+      <p style={{ color: isOnline ? "green" : "red" }}>
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
